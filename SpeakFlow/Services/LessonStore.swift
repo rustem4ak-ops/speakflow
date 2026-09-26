7f021b62f64257a638c04ce65ce5b3a1313bedda
rustem4ak-ops <rustem4ak@gmail.com>
2026-09-26 08:55:03 +0300

    Add local lesson store

--- SpeakFlow/Services/LessonStore.swift
+++ SpeakFlow/Services/LessonStore.swift
@@ -1,22 +1,13 @@
 import Foundation
+import SwiftUI
 
 final class LessonStore: ObservableObject {
     @Published private(set) var courses: [Course] = []
 
     init() {
-        load()
-    }
-
-    private func load() {
-        guard let url = Bundle.main.url(forResource: "lessons", withExtension: "json") else {
-            return
-        }
-        do {
-            let data = try Data(contentsOf: url)
-            courses = try JSONDecoder().decode([Course].self, from: data)
-        } catch {
-            courses = []
-            print("LessonStore error: \(error)")
-        }
+        guard let url = Bundle.main.url(forResource: "lessons", withExtension: "json"),
+              let data = try? Data(contentsOf: url),
+              let decoded = try? JSONDecoder().decode([Course].self, from: data) else { return }
+        courses = decoded
     }
 }
